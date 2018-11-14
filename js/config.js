12ef79964b8d0de091ef44a3038ab23056db76bf
evanmillican <evanmill@live.unc.edu>
2018-11-14 12:45:19 -0500

    first commit

---
+++ js/config.js
@@ -0,0 +1,3 @@
+var config = {
+  MY_KEY = "cfc8e67278ab4b87aab2d9429f5acac0"
+}
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function(){
+  console.log('scripts loaded');
+
+  var myKey = config.MY_KEY;
+
+  var url = 'https://newsapi.org/v2/everything?q=bitcoin&from=2018-10-14&sortBy=publishedAt&apiKey=cfc8e67278ab4b87aab2d9429f5acac0' + myKey;
+
+});
